f9f15e074c06088290caf671ef48c570ca52f5ea
Deepthi1003 <dtchokka@gmail.com>
2019-11-06 17:24:34 -0600

    commit

--- models/section.js
+++ models/section.js
@@ -1,49 +1,57 @@
+/**
+*  Developer model
+*  Describes the characteristics of each attribute in a developer resource.
+*
+* @author Denise Case <denisecase@gmail.com>
+* @requires mongoose
+*
+*/
+
 const mongoose = require('mongoose')
 
-const DeveloperSchema = new mongoose.Schema({
-
-  _id: {
-    type: String,
-    required: true
-  },
-  SectionNumber: {
-    type: String,
-    minlength: 3,
-    maxlength: 100,
-    required: false,
-  },
-  Days: {
-    type: String,
-    minlength: 3,
-    maxlength: 100,
-    required: false,
-  },
-  StartTime: {
-      type: Number,
-      minlength: 1,
-      maxlength: 4,
-      required: true,
-  },
-  RoomNumber: {
-    type: String,
-    minlength: 5,
-    maxlength: 100,
-    required: true,
-    unique: true  
-  },
-  InstructorID: {
-    type: String,
-    minlength: 5,
-    maxlength: 100,
-    required: true,
-    unique: true 
-  },
-  CourseID: {
-    type: String,
-    minlength: 5,
-    maxlength: 100,
-    required: true,
-    unique: true 
-  }
-
-})
+const sectionSchema = new mongoose.Schema({
+    _id: {
+        type: Number,
+        required: true
+        
+      },
+      SectionNumber: {
+        type: String,
+        minlength: 2,
+        maxlength: 2,
+        required: true,
+        unique: true
+      },
+      Days: {
+        type: String,
+        minlength: 2,
+        maxlength: 3,
+        required: true
+        
+        
+      },
+      StartTime: {
+        type: String,
+        minlength: 4,
+        maxlength: 4,
+        required: false
+       
+      },
+      RoomNumber: {
+        type: String,
+        required: true
+       
+      },
+      InstructorID: {
+        type: Number,
+        required: true
+      
+      },
+      CourseID: {
+        type: Number,
+        required: true
+        
+      }
+    })
+    module.exports = mongoose.model('section', sectionSchema)
+    
