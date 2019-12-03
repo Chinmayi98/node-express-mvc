44b9a9c891b43a80ebad97b329f77d5bfd38289d
Chokka <S536974@nwmissouri.edu>
2019-12-03 15:40:38 -0600

    adding necessary changes to controller

--- controllers/student.js
+++ controllers/student.js
@@ -7,113 +7,148 @@
 */
 const express = require('express')
 const api = express.Router()
+const LOG = require('../utils/logger.js')
 const Model = require('../models/student.js')
-const find = require('lodash.find')
-const notfoundstring = 'Could not find student with id='
+const notfoundstring = 'student not found'
 
 // RESPOND WITH JSON DATA  --------------------------------------------
-
 // GET all JSON
 api.get('/findall', (req, res) => {
-  res.setHeader('Content-Type', 'application/json')
-  const data = req.app.locals.students.query
-  res.send(JSON.stringify(data))
+  LOG.info(`Handling /findall ${req}`)
+  Model.find({}, (err, data) => {
+    res.json(data)
+  })
 })
 
 // GET one JSON by ID
 api.get('/findone/:id', (req, res) => {
-  res.setHeader('Content-Type', 'application/json')
+  LOG.info(`Handling /findone ${req}`)
   const id = parseInt(req.params.id)
-  const data = req.app.locals.students.query
-  const item = find(data, { _id: id })
-  if (!item) { return res.end(notfoundstring + id) }
-  res.send(JSON.stringify(item))
+  Model.find({ _id: id }, (err, results) => {
+    if (err) { return res.end(notfoundstring) }
+    res.json(results[0])
+  })
 })
 
 // RESPOND WITH VIEWS  --------------------------------------------
 
-// GET to this controller base URI (the default)
+// GET request to base page.
 api.get('/', (req, res) => {
-  res.render('student/index.ejs', {
-    students: req.app.locals.students.query
+  LOG.info(`Handling GET / ${req}`)
+  Model.find({}, (err, data) => {
+    res.locals.students = data
+    res.render('student/index.ejs')
   })
 })
 
-// GET create
+// GET to create page
 api.get('/create', (req, res) => {
-  res.render('student/create', {
-    students: req.app.locals.students.query,
-    student: new Model()
+  LOG.info(`Handling GET /create ${req}`)
+  Model.find({}, (err, data) => {
+    res.locals.students = data
+    res.locals.student = new Model()
+    res.render('student/create')
   })
 })
 
-// GET /delete/:id
+// GET to details page
 api.get('/delete/:id', (req, res) => {
+  LOG.info(`Handling GET /delete/:id ${req}`)
   const id = parseInt(req.params.id)
-  const data = req.app.locals.students.query
-  const item = find(data, { _id: id })
-  if (!item) { return res.end(notfoundstring + id) }
-  res.render('student/delete', {
-    student: item
+  Model.find({ _id: id }, (err, results) => {
+    if (err) { return res.end(notfoundstring) }
+    LOG.info(`RETURNING VIEW FOR ${JSON.stringify(results)}`)
+    res.locals.student = results[0]
+    return res.render('student/delete.ejs')
   })
 })
 
-// GET /details/:id
+// GET to create page
 api.get('/details/:id', (req, res) => {
+  LOG.info(`Handling GET /details/:id ${req}`)
   const id = parseInt(req.params.id)
-  const data = req.app.locals.students.query
-  const item = find(data, { _id: id })
-  if (!item) { return res.end(notfoundstring + id) }
-  res.render('student/details', {
-    student: item
+  Model.find({ _id: id }, (err, results) => {
+    if (err) { return res.end(notfoundstring) }
+    LOG.info(`RETURNING VIEW FOR ${JSON.stringify(results)}`)
+    res.locals.student = results[0]
+    return res.render('student/details.ejs')
   })
 })
 
-// GET one
+// GET to create page
 api.get('/edit/:id', (req, res) => {
+  LOG.info(`Handling GET /edit/:id ${req}`)
   const id = parseInt(req.params.id)
-  const data = req.app.locals.students.query
-  const item = find(data, { _id: id })
-  if (!item) { return res.end(notfoundstring + id) }
-  res.render('student/edit', {
-    student: item
+  Model.find({ _id: id }, (err, results) => {
+    if (err) { return res.end(notfoundstring) }
+    LOG.info(`RETURNING VIEW FOR${JSON.stringify(results)}`)
+    res.locals.student = results[0]
+    return res.render('student/edit.ejs')
   })
 })
 
-// HANDLE EXECUTE DATA MODIFICATION REQUESTS --------------------------------------------
+// RESPOND WITH DATA MODIFICATIONS  -------------------------------
 
-// POST new
+// post new
 api.post('/save', (req, res) => {
-  console.info(`Handling POST ${req}`)
-  console.debug(JSON.stringify(req.body))
+  LOG.info(`Handling POST ${req}`)
+  LOG.debug(JSON.stringify(req.body))
   const item = new Model()
-  console.info(`NEW ID ${req.body._id}`)
+  LOG.info(`NEW ID ${req.body._id}`)
   item._id = parseInt(req.body._id)
-  item.Given = req.body.Given
-  item.Family = req.body.Family
   item.Email = req.body.Email
+  item.Given = req.body.Given
+  item.family = req.body.family
   item.GPA = req.body.GPA
-  item.Github = req.body.Github
   item.Website = req.body.Website
-  item.SectionID = req.body.SectionID
-  item.Hobby = req.body.Hobby
-  res.send(`THIS FUNCTION WILL SAVE A NEW student  ${JSON.stringify(item)}`)
+  item.GitHub = req.body.GitHub
+   item.Section = req.body.Section
+  item.save((err) => {
+    if (err) { return res.end('ERROR: student could not be saved') }
+    LOG.info(`SAVING NEW student ${JSON.stringify(item)}`)
+    return res.redirect('/student')
+  })
 })
 
-// POST update with id
+// THESE ARE POST REQUEST ROUTES, WILL ONLY REPLY TO POST REQUEST.
+// ABOVE ROUTING IS GET REQUEST FOR CERTAIN IDS
+
+// post save w/ id
 api.post('/save/:id', (req, res) => {
-  console.info(`Handling SAVE request ${req}`)
+  LOG.info(`Handling SAVE request ${req}`)
   const id = parseInt(req.params.id)
-  console.info(`Handling SAVING ID=${id}`)
-  res.send(`THIS FUNCTION WILL SAVE CHANGES TO AN EXISTING student with id=${id}`)
+  LOG.info(`Handling SAVING ID=${id}`)
+  Model.updateOne({ _id: id },
+    { // use mongoose field update operator $set
+      $set: {
+        Email: req.body.Email,
+        Given: req.body.Given,
+        family: req.body.family,
+         GPA: req.body.GPA,
+         Website: req.body.Website,
+        GitHub: req.body.GitHub,
+         Section: req.body.Section,
+      }
+    },
+    (err, item) => {
+      if (err) { return res.end(notfoundstring) }
+      LOG.info(`ORIGINAL VALUES ${JSON.stringify(item)}`)
+      LOG.info(`UPDATED VALUES: ${JSON.stringify(req.body)}`)
+      LOG.info(`SAVING UPDATED student ${JSON.stringify(item)}`)
+      return res.redirect('/student')
+    })
 })
 
-// DELETE id (uses HTML5 form method POST)
+// delete by id
 api.post('/delete/:id', (req, res) => {
-  console.info(`Handling DELETE request ${req}`)
+  LOG.info(`Handling DELETE request ${req}`)
   const id = parseInt(req.params.id)
-  console.info(`Handling REMOVING ID=${id}`)
-  res.send(`THIS FUNCTION WILL DELETE FOREVER THE EXISTING student with id=${id}`)
+  LOG.info(`Handling REMOVING ID=${id}`)
+  Model.remove({ _id: id }).setOptions({ single: true }).exec((err, deleted) => {
+    if (err) { return res.end(notfoundstring) }
+    console.log(`Permanently deleted item ${JSON.stringify(deleted)}`)
+    return res.redirect('/student')
+  })
 })
 
 module.exports = api
